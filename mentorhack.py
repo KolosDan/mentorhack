d8364ff5b8bddbe4bd758bfdc99dd9113a137be8
KolosDan <dan.danya99@gmail.com>
2018-02-15 16:58:52 +0500

    Smart-contract

--- mentorhack.py
+++ mentorhack.py
@@ -1,12 +1,105 @@
-from flask import Flask
-from flask import r
+from flask import Flask, request
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+import vk
+import re
+import json
+from collections import Counter
+import web
+
 app = Flask(__name__)
+
+db = MongoClient().mentorhack
+
+session = vk.Session(access_token='7aa240bd62486472f05c8457cc24b63fec822359d30cbb79dcce625b76b09b16e7faa83ce547ee0f34d1e')
+
+api = vk.API(session)
+
+'''
+Верификация: контроль за компетенциями от независимых наблюдателей GRAPHENE!!!!!
+
+Сосредоточиться на алгоритмах поиска и сравнения.
+'''
+
+def update_personal(id,input):
+    db.person.update_one({'_id': ObjectId(id)}, {'$set': input})
+
+
+def get_vk(user_id):
+    link = db.person.find_one({'_id': ObjectId(user_id)})['vk'].split('/')[-1]
+    vk_info = api.users.get(user_ids=link,
+                            fields=['status', 'activities', 'interests', 'music', 'movies', 'tv', 'books', 'games',
+                                    'about', 'quotes', 'personal'])[0]
+    del vk_info['first_name']
+    del vk_info['last_name']
+    groups = []
+    api.wall.get(domain=link)
+    for i in api.groups.getById(group_ids=api.groups.get(user_id=vk_info['uid']),
+                                fields=['description', 'activity', 'status']):
+        try:
+            description = i['description']
+        except:
+            description = ''
+        try:
+            status = i['status']
+        except:
+            status = ''
+        groups.append({'name': i['name'], 'description': re.sub('<br>', ' ', description), 'status': status})
+    vk_info['groups'] = groups
+    text = ''
+    for i in vk_info['wall']:
+        text += i
+    for i in vk_info['groups']:
+        text += i['description']
+    tag_arr = re.findall(r'(\w+)', re.sub('.', ' ',re.sub(',', ' ', re.sub('<br>', ' ', text))))
+
+    cnt = Counter()
+    for word in tag_arr:
+        if len(word) > 2:
+            cnt[word] += 1
+            popular = cnt.most_common(100)
+
+    ex = ['com', 'для', 'что', 'это', 'https', 'http', 'как', 'или', 'группы', 'группа', 'нас', 'уже', 'все', 'всех',
+          'так', 'если', "здесь", "только"]
+
+    top = []
+    for i in popular:
+        top.append(i[0])
+
+    tags = [x for x in top if x not in ex]
+
+    db.vk_info.insert_one({'user_id':user_id, 'vk_info':vk_info, 'tags': tags})
+
+
+def compare_personal(id1,id2):
+    user1 = db.person.find_one({'user_id':ObjectId(id1)})
+    user2 = db.person.find_one({'user_id':ObjectId(id2)})
+    temp1 = []
+    temp2 = []
+    for i in user1['groups']:
+        temp1.append(i['name'])
+    for i in user2['groups']:
+        temp2.append(i['name'])
+    com_groups = list(set(temp1) & set(temp2))
+
+
+def get_tags(user_id):
+    pass
+
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return 'Hola'
 
+
+@app.route('/api/users/create', methods=['POST'])
+def create_user():
+    input = request.get_json(force=True)
+    print(input)
+    id = json.dumps({'token': str(db.users.insert(input))})
+
+    return id
 
 if __name__ == '__main__':
     app.run()
---
+++ mentorhack.sol
@@ -0,0 +1,224 @@
+pragma solidity ^0.4.0;
+//import "github.com/oraclize/ethereum-api/oraclizeAPI.sol";
+
+// библиотека для математических вычислений 
+library SafeMath {
+    
+  function mul(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a * b;
+    assert(a == 0 || c / a == b);
+    return c;
+  }
+ 
+  function div(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a / b;
+    return c;
+  }
+ 
+  function sub(uint256 a, uint256 b) internal constant returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+ 
+  function add(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+  
+}
+ 
+// Контракт модификаторов и функций доступа
+contract permissions
+{
+    address owner = msg.sender;
+    address admin;
+    
+    function changeOwner(address newOwner) onlyOwner //Смена владельца
+    {
+        owner = newOwner;
+    }
+
+    modifier onlyOwner() // Модификатор доступа "только владелец"
+    {
+        require(msg.sender == owner);
+        _;
+    }
+    modifier onlyAdmin() // Модификатор доступа "администратор и владелец"
+    {
+        require(msg.sender == admin || msg.sender == owner);
+        _;
+    }
+    function addAdmin(address _admin) // Смена (или) добавление администратора 
+    {
+        admin = _admin;
+    }
+}
+
+// Главный контракт
+contract MentorCoin is permissions
+{
+    using SafeMath for uint256;
+ 
+    string public constant name = "MentorCoin";
+    string public constant symbol = "MNT";
+    uint8 public constant decimals = 0;
+    bool public canTransfer = true ;
+
+    uint internal totalSupply = 13372280;
+
+    mapping (address => uint256) balances;
+    mapping (address => mapping (address => uint256)) allowed;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint _value);
+    
+
+    function balanceOf(address _owner) constant returns (uint256 balance) // проверка баланса
+    {
+        return balances[_owner];
+    }
+
+    function transfer(address _to, uint256 _value) returns (bool success) // перевод токенов
+    {
+        assert(canTransfer == true);
+    if (balances[msg.sender]>=_value && _value >0 && balances[_to].add(_value) >= balances[_to])
+    {
+        balances[msg.sender]= balances[msg.sender].sub(_value);
+        balances[_to]= balances[_to].add(_value);
+        Transfer(msg.sender, _to, _value);
+        return true;
+    }
+    else {return false;}
+    }
+    function startTransfer() onlyOwner // возможность переводить
+    {
+        canTransfer = true;
+    }
+    function stopTransfer() onlyOwner // остановка любых операций перевода
+    {
+        canTransfer = false;
+    }
+    function burn(address who, uint value) onlyAdmin // сжигаем токены с адреса
+    {
+        if (value<=balances[who])
+        {
+            balances[who] = balances[who].sub(value);
+        }
+        else{}
+    }
+
+    function transferFrom(address _from, address _to, uint _value) returns (bool success) // перевод с адреса на адрес
+    {
+    if( allowed[_from][msg.sender] >= _value &&
+    balances[_from] >= _value
+    && balances[_to].add(_value) >= balances[_to]) 
+    {
+        allowed[_from][msg.sender]=  allowed[_from][msg.sender].sub(_value);
+        balances[_from]= balances[_from].sub(_value);
+        balances[_to]= balances[_to].add(_value);
+        Transfer(_from, _to, _value);
+        return true;
+    }
+        return false;
+    }
+
+    function approve(address _spender, uint _value) returns (bool success) // получение доступа для снятия со счета
+    {
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) constant returns (uint remaining) // просмотр разрешенных для снятия токенов
+    {
+        return allowed[_owner][_spender];
+
+    }
+
+    function mint(address _to, uint256 _value) onlyOwner // выпуск новых токенов
+    {
+        assert(totalSupply.add(_value) >= totalSupply && balances[_to].add(_value) >= balances[_to]);
+        balances[_to]= balances[_to].add(_value);
+        totalSupply= totalSupply.add(_value);
+    }
+
+}
+// Контракт для реализации продажи токенов
+contract sales is zharcoin
+{
+    uint coef = 1000000000000;
+    uint stage1 = 1509753600;
+    uint stage2 = 1510012800;
+    uint stage3 = 1510704000;
+    uint public bonus = 0;
+    bool public canSale = true;
+    uint public coinCount = totalSupply;
+    
+    function() external payable // fallback функция
+    {   
+        if(coinCount>0){
+        if(canSale==true){
+        owner.transfer(msg.value);
+        uint buy = msg.value.div(coef);
+        if(bonus == 0 && buy < totalSupply && buy<coinCount)
+        {
+            totalSupply= totalSupply.sub(buy);
+            coinCount = coinCount.sub(buy);
+            balances[msg.sender]= balances[msg.sender].add(buy);
+        }
+        
+        else if(buy < totalSupply && buy< coinCount)
+        {  
+            totalSupply= totalSupply.sub((buy.add(buy.div(bonus))));
+            coinCount= coinCount.sub((buy.add(buy.div(bonus))));
+            balances[msg.sender]= balances[msg.sender].add((buy.add(buy.div(bonus))));
+        }
+    else {msg.sender.transfer(msg.value);}
+        } 
+    else{}
+    }}
+    
+    function changeCoef(uint newCoef) onlyAdmin //коэффициент для смены стоимости токена
+    {
+        coef = newCoef;
+    }
+    
+    function isSale(uint _stage1, uint bonus1, uint _stage2, uint bonus2, uint _stage3) onlyAdmin // распродажа токенов в несколько этапов
+    {
+        stage1 = _stage1;
+        stage2 = _stage2;
+        stage3 = _stage3;
+        if(now >= stage1 && now <= stage2)
+    {
+        bonus = bonus1;
+    }
+        
+        if(now >= stage2 && now <= stage3)
+    {
+        bonus = bonus2;
+    }
+            
+    }
+    
+    function getRate() public constant returns(uint) // возврат текущего курса токена к эфиру
+    {
+        return 1000000000000000000/coef;
+    }
+    
+    function chngCoinNumber(uint num, uint _bonus) onlyAdmin // смена количества распродаваемых токенов
+    {
+        coinCount = num;
+        bonus = _bonus;
+    }
+    
+    function startSale() onlyOwner // начало продаж
+    {
+        canSale = true;
+    }
+    function stopSale() onlyOwner // остановка продаж
+    {
+        canSale = false;
+    }
+    
+}
